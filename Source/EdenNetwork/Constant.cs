5117d142a072ddeebde7986e7a3e7b8bdbad8a41
beatheat <oh@beathe.at>
2023-02-17 15:01:52 +0900

    refactor: 공유 상수 분리

---
+++ Source/EdenNetwork/Constant.cs
@@ -0,0 +1,15 @@
+﻿namespace EdenNetwork
+{
+	public static class Constant
+	{
+		/// <summary>
+		/// Constant : size of read buffer for each client 8KB
+		/// </summary>
+		public const int BUF_SIZE = 8 * 1024;
+		/// <summary>
+		/// Constant : constant for receive request packet
+		/// </summary>
+		public const string REQUEST_PREFIX = "*r*";
+	}
+}
+
